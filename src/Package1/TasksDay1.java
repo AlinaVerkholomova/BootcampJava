c63252b009f068f69131a55fd7713a2a9978db35
AlinaVerkholomova <alina.verkholomova@gmail.com>
2022-06-28 14:22:04 +0300

    Task of the first day of Bootcamp

---
+++ src/Package1/TasksDay1.java
@@ -0,0 +1,20 @@
+package Package1;
+
+public class TasksDay1 {
+    public static void main(String[] args) {
+        byte myByte = 1;
+        short myShort = -1;
+        int myInt = 20;
+        long myLong = 201762192;
+        float myFloat = 10.87f;
+        double myDouble = 3.23;
+        char myChar = 'a';
+        boolean myBool = false;
+
+        System.out.println(myShort + " " + " " + myByte + " " + myInt
+                + " " + myLong + " " + myFloat + " " + myDouble +
+                " " + myChar + " " + myBool);
+
+    }
+}
+
