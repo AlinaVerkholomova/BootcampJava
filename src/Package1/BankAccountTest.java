e2fd1edb11333b94d44d66a770e6166333b78aaf
AlinaVerkholomova <alina.verkholomova@gmail.com>
2022-07-01 11:39:38 +0300

    Modified BankAccounttest.java

--- src/Package1/BankAccountTest.java
+++ src/Package1/BankAccountTest.java
@@ -1,6 +1,5 @@
 package Package1;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,28 +7,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class BankAccountTest {
     private static BankAccount bankAccount;
 
-    @BeforeAll
-    protected static void setUp() {
-        bankAccount = new BankAccount(1000);
-    }
-
     @Test
     void depositShouldBe1200() {
+        bankAccount = new BankAccount(1000);
         assertEquals( 1200, bankAccount.deposit(200));
     }
 
     @Test
     void depositShouldBeZero(){
+        bankAccount = new BankAccount(1000);
         assertEquals(0, bankAccount.deposit(5100));
     }
 
     @Test
     void withdrawShouldBe800() {
+        bankAccount = new BankAccount(1000);
         assertEquals( 800, bankAccount.withdraw(200));
     }
 
     @Test
     void withdrawShouldBeZero() {
+        bankAccount = new BankAccount(1000);
         assertEquals( 0, bankAccount.withdraw(1100));
 
     }
